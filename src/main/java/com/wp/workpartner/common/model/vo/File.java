9f7929cdcec574ce704ee08a3cc4983679989736
uhee <103394624+iamuheee@users.noreply.github.com>
2022-09-08 16:24:46 +0900

    Fix : 메소드 수정
    - Fix : 파일이 참조하는 게시글 번호를 받지 않도록 수정 (게시글 작성할 때에는 새로 등록되는 해당 게시글 번호를 받아오기 힘들어서 일단 뺌)

--- src/main/java/com/wp/workpartner/common/model/vo/File.java
+++ src/main/java/com/wp/workpartner/common/model/vo/File.java
@@ -26,11 +26,10 @@ public class File {
 	private String fileLevel;
 	private String fileStatus;
 	
-	public static File uploadFile(MultipartFile upfile, int refNo, String saveFilePath) {
+	public static File uploadFile(MultipartFile upfile, String saveFilePath) {
 		// fileNo, fileCategory, fileUploadDate, fileLevel, fileStatus는 
 		// SQL문에서 직접 작성하기때문에 여기서 추가하지 않음
 		File file = new File();
-		file.refNo = refNo;
 		file.fileOriginName = upfile.getOriginalFilename();
 		file.fileChangeName = saveFilePath.substring(saveFilePath.lastIndexOf('/') + 1, saveFilePath.lastIndexOf('.'));
 		file.filePath = saveFilePath;
