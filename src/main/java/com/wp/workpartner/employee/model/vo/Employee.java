1a24a4b0dfdbc0c6b9503e09c98a6cafe1bc92c7
uhee <103394624+iamuheee@users.noreply.github.com>
2022-09-10 17:24:55 +0900

    Create : Employee VO 객체 추가
    - 필드명은 테이블의 컬럼명 그대로 낙타표기법 지켜 작성했지만 제가 주로 쓰는 테이블이 아니다보니 틀렸을 수도 ㅎㅎ.... 해당 테이블 자주 쓰시는 태은님, 태민님께서 보시고 편하신 대로 수정 부탁드려요!!

---
+++ src/main/java/com/wp/workpartner/employee/model/vo/Employee.java
@@ -0,0 +1,39 @@
+package com.wp.workpartner.employee.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Employee {
+	
+	private String empNo;
+	private String empId;
+	private String empPwd;
+	private String empPhone;
+	private String empExtension;
+	private String empEmail;
+	private String empExEmail;
+	private String empEnrollDate;
+	private String empRetireDate;
+	private String empModifyDate;
+	private String empStatus;
+	private String empProfile;
+	private String empAccStatus;
+	private String empNtAdmin;
+	private String depCd;
+	private String posCd;
+	private String empTenure;
+	private String empSigUse;
+	private String empMoveDate;
+	private String empHolTake;
+	private String empHolUse;
+	
+
+}
