ac70148d7bd1064dd702bd17247471d56ffa36a9
uhee <103394624+iamuheee@users.noreply.github.com>
2022-09-10 21:39:31 +0900

    Create : To do 관련 VO객체, mapper 추가

---
+++ src/main/java/com/wp/workpartner/todo/model/vo/Todo.java
@@ -0,0 +1,23 @@
+package com.wp.workpartner.todo.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Todo {
+	private String todoNo;
+	private String empNo;
+	private String categoryNo;
+	private String categoryTitle;
+	private String doneYN;
+	private String todoDate;
+	private String color;
+
+}
