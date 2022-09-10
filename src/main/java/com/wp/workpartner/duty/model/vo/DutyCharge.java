6f0e607d792324772860f9ea5f3f1c8a2ce6d393
uhee <103394624+iamuheee@users.noreply.github.com>
2022-09-10 17:25:45 +0900

    Create : DutyCharge VO 객체 추가, config.xml에 type Alias 추가

---
+++ src/main/java/com/wp/workpartner/duty/model/vo/DutyCharge.java
@@ -0,0 +1,18 @@
+package com.wp.workpartner.duty.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class DutyCharge {
+	private int dutyNo;
+	private String empICNo;
+	private String empICName;
+}
