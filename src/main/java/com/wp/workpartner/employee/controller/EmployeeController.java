3098e07631786af4327e86577f0562aac966c169
staerry <chbb1022@gmail.com>
2022-09-11 22:27:04 +0900

    Update : 마이페이지, 사용자 등록, 사용자 조회 페이지 연결 메소드 작성

--- src/main/java/com/wp/workpartner/employee/controller/EmployeeController.java
+++ src/main/java/com/wp/workpartner/employee/controller/EmployeeController.java
@@ -3,18 +3,22 @@ package com.wp.workpartner.employee.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * @author ten
+ *
+ */
 @Controller
 public class EmployeeController {
 
 	/**
 	 * @author	: Taeeun Park
 	 * @date	: 2022. 9. 11.
-	 * @method	: 로그인 페이지로 이동하는 요청 처리 메소드
+	 * @method	: 로그인 페이지 이동 요청 처리 메소드
 	 * @return	: String "common/loginForm"
 	 */
 	@RequestMapping("loginForm.em") 
 	public String loginForm() {
-		return "common/loginForm";
+		return "employee/loginForm";
 	}
 	
 	/**
@@ -22,36 +26,66 @@ public class EmployeeController {
 	 * @author	: Taeeun Park
 	 * @date	: 2022. 9. 11.
 	 * @method	: 로그아웃 요청 처리 메소드
-	 * @return	: String
+	 * @return	: String "employee/loginForm"
 	 */
 	@RequestMapping("logout.em")
 	public String logoutEmployee() {
-		return "common/loginForm";
+		return "employee/loginForm";
 	}
 	
 	/**
 	 * @author	: Taeeun Park
 	 * @date	: 2022. 9. 11.
-	 * @method	: 아이디 찾기 페이지로 이동하는 요청 처리 메소드
-	 * @return	: String
+	 * @method	: 아이디 찾기 페이지 요청 처리 메소드
+	 * @return	: String "employee/idSelectForm";
 	 */
 	@RequestMapping("idSelectForm.em")
 	public String idSelectForm() {
-		return "common/idSelectForm";
+		return "employee/idSelectForm";
 	}
 	
 	/**
 	 * @author	: Taeeun Park
 	 * @date	: 2022. 9. 11.
-	 * @method	: 임시 비밀번호 발급 페이지로 이동하는 요청 처리 메소드
-	 * @return	: String
+	 * @method	: 임시 비밀번호 발급 페이지 요청 처리 메소드
+	 * @return	: String "employee/pwdUpdateForm";
 	 */
 	@RequestMapping("pwdUpdateForm.em")
 	public String pwdUpdateForm() {
-		return "common/pwdUpdateForm";
+		return "employee/pwdUpdateForm";
 	}
 	
+	/**
+	 * @author	: Taeeun Park
+	 * @date	: 2022. 9. 11.
+	 * @method	: 마이페이지 요청 처리 메소드
+	 * @return	: String "employee/myPage";
+	 */
+	@RequestMapping("mypage.em")
+	public String myPage() {
+		return "employee/myPage";
+	}
 	
+	/**
+	 * @author	: Taeeun Park
+	 * @date	: 2022. 9. 11.
+	 * @method	: 사용자 등록 페이지 요청 처리 메소드
+	 * @return	: String "employee/employeeInsertForm";
+	 */
+	@RequestMapping("insertForm.em")
+	public String empInsertForm() {
+		return "employee/employeeInsertForm";
+	}
 	
+	/**
+	 * @author	: Taeeun Park
+	 * @date	: 2022. 9. 11.
+	 * @method	: 사용자 조회 요청 처리 메소드 
+	 * @return	: String "employee/employeeListView"
+	 */
+	@RequestMapping("list.em")
+	public String selectEmpList() {
+		return "employee/employeeListView";
+	}
 	
 }
