87226a55894222e749e45830d0afc886a38816f9
jimi0512 <wwlaal0512@naver.com>
2022-09-13 20:50:21 +0900

    feat: addressListView ajax
    -add: 부서목록 ajax
    -add: 내연락처 목록 ajax
    -add : 각 테이블 조회 및 페이징처리 ajax

--- src/main/java/com/wp/workpartner/address/controller/AddressController.java
+++ src/main/java/com/wp/workpartner/address/controller/AddressController.java
@@ -1,26 +1,93 @@
 package com.wp.workpartner.address.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.google.gson.Gson;
 import com.wp.workpartner.address.model.service.AddressService;
+import com.wp.workpartner.address.model.vo.Department;
+import com.wp.workpartner.address.model.vo.MyAddress;
+import com.wp.workpartner.address.model.vo.MyGroup;
+import com.wp.workpartner.common.model.vo.PageInfo;
+import com.wp.workpartner.common.template.Pagination;
+import com.wp.workpartner.employee.model.vo.Employee;
 
 @Controller
 public class AddressController {
 		
 	@Autowired
-	private AddressService bService;
+	private AddressService adService;
 	
+	// 주소록list
 	@RequestMapping("list.ad")
 	private String selectAddressList() {
 		return "address/addressListView";
 	}
+		
+	// 부서 list용 ajax
+	@ResponseBody
+	@RequestMapping(value="depList.ad", produces="application/json; charset=utf-8")
+	public String ajaxSelectDepartmentList() {
+		ArrayList<Department> list = adService.selectDepList();
+		//System.out.println(list);
+		return new Gson().toJson(list);
+	}
 	
-	// 부서	
-//	@ResponseBody
-//	@RequestMapping(value="deplist.ad", produces="application/json; charset=utf-8")
-//	public String ajaxSelectDepartmentList() {
-//		ArrayList<Department> list = 
-//	}
+	// 내연락처 목록용 ajax
+	@ResponseBody
+	@RequestMapping(value="gpList.ad", produces="application/json; charset=utf-8")
+	public String ajaxSelectGroupList(String employeeNo) {
+		ArrayList<MyGroup> list = adService.selectGpList(employeeNo);
+		return new Gson().toJson(list);
+	}
+	
+	// 부서별 사원테이블조회용 ajax
+	@ResponseBody
+	@RequestMapping(value="depTb.ad", produces="application/json; charset=utf-8")
+	public String ajaxSelectEmpAddList(int depCd, @RequestParam(value="cpage", defaultValue="1")int currentPage) {
+		
+		int listCount = adService.selectEmpAdListCount(depCd);
+		PageInfo pi = Pagination.getPageInfo(listCount, currentPage, 3, 10);				
+		ArrayList<Employee> list = adService.selectEmpAdList(depCd, pi);		
+		
+		HashMap<String, Object> hm = new HashMap<>();
+		hm.put("list", list);
+		hm.put("pi", pi);
+		return new Gson().toJson(hm);		
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="myAdTb.ad", produces="application/json; charset=utf-8")
+	public String ajaxSelectMyAddList(int groupNo, @RequestParam(value="cpage", defaultValue="1")int currentPage) {
+		
+		int listCount = adService.selectMyAddCount(groupNo);
+		PageInfo pi = Pagination.getPageInfo(listCount, currentPage, 3, 10);		
+		ArrayList<MyAddress> list = adService.selectMyAddList(groupNo, pi);
+		
+		HashMap<String, Object> hm = new HashMap<>();
+		hm.put("list", list);
+		hm.put("pi", pi);
+		return new Gson().toJson(hm);		
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="starList.ad", produces="application/json; charset=utf-8")
+	public String ajaxSelectStarList(String empNo, @RequestParam(value="cpage", defaultValue="1")int currentPage) {
+		
+		int listCount = adService.selectStarAdCount(empNo);
+		PageInfo pi = Pagination.getPageInfo(listCount, currentPage, 3, 2);
+		ArrayList<MyAddress> list = adService.selectStarList(empNo, pi);
+		
+		HashMap<String, Object> hm = new HashMap<>();
+		hm.put("list", list);
+		hm.put("pi", pi);
+		return new Gson().toJson(hm);
+		
+	}
 }
--- src/main/java/com/wp/workpartner/address/model/dao/AddressDao.java
+++ src/main/java/com/wp/workpartner/address/model/dao/AddressDao.java
@@ -1,8 +1,62 @@
 package com.wp.workpartner.address.model.dao;
 
+import java.util.ArrayList;
+
+import org.apache.ibatis.session.RowBounds;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.wp.workpartner.address.model.vo.Department;
+import com.wp.workpartner.address.model.vo.MyAddress;
+import com.wp.workpartner.address.model.vo.MyGroup;
+import com.wp.workpartner.common.model.vo.PageInfo;
+import com.wp.workpartner.employee.model.vo.Employee;
 
 @Repository
 public class AddressDao {
-
+	
+	public ArrayList<Department> selectDepList(SqlSessionTemplate sqlSession){
+		return (ArrayList)sqlSession.selectList("addressMapper.selectDepList");
+	}
+	
+	public ArrayList<MyGroup> selectGpList(SqlSessionTemplate sqlSession, String empNo){
+		return (ArrayList)sqlSession.selectList("addressMapper.selectGpList", empNo);
+	}
+	
+	public ArrayList<Employee> selectEmpAdList(SqlSessionTemplate sqlSession, int depCd, PageInfo pi){
+		
+		int limit = pi.getBoardLimit();
+		int offset = (pi.getCurrentPage() - 1) * limit; // 몇개 건너뛰고 조회할지
+		RowBounds rowBounds = new RowBounds(offset, limit);
+		
+		return (ArrayList)sqlSession.selectList("addressMapper.selectEmpAdList", depCd, rowBounds);
+	}
+	
+	public int selectEmpAdListCount(SqlSessionTemplate sqlSession, int depCd) {
+		return sqlSession.selectOne("addressMapper.selectEmpAdListCount", depCd);
+	}
+	
+	public int selectMyAddCount(SqlSessionTemplate sqlSession, int groupNo) {
+		return sqlSession.selectOne("addressMapper.selectMyAddCount", groupNo);
+	}
+	
+	public ArrayList<MyAddress> selectMyAddList(SqlSessionTemplate sqlSession, int groupNo, PageInfo pi){
+		
+		int limit = pi.getBoardLimit();
+		int offset = (pi.getCurrentPage() - 1) * limit;
+		RowBounds rowBounds = new RowBounds(offset, limit);
+		return (ArrayList)sqlSession.selectList("addressMapper.selectMyAddList", groupNo, rowBounds);
+	}
+	
+	public int selectStarAdCount(SqlSessionTemplate sqlSession, String empNo) {
+		return sqlSession.selectOne("addressMapper.selectStarAdCount", empNo);		
+	}
+	
+	public ArrayList<MyAddress> selectStarList(SqlSessionTemplate sqlSession, String empNo, PageInfo pi){
+		
+		int limit = pi.getBoardLimit();
+		int offset = (pi.getCurrentPage() - 1) * limit;
+		RowBounds rowBounds = new RowBounds(offset, limit);
+		return (ArrayList)sqlSession.selectList("addressMapper.selectStarList", empNo, rowBounds);
+	}
 }
