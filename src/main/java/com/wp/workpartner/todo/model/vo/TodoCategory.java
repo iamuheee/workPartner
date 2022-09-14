0d381620dffd05feab1562f4cf35a175b4d5c950
uhee <103394624+iamuheee@users.noreply.github.com>
2022-09-14 13:50:51 +0900

    Update : TodoCategory VO객체 추가, 관련 mapper파일 수정

---
+++ src/main/java/com/wp/workpartner/todo/model/vo/TodoCategory.java
@@ -0,0 +1,24 @@
+package com.wp.workpartner.todo.model.vo;
+
+import java.util.ArrayList;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class TodoCategory {
+	private String empNo;
+	private String categoryNo;
+	private String categoryTitle;
+	private String color;
+	
+	// 카테고리별 모든 To do를 담는 ArrayList<Todo>
+	private ArrayList<Todo> todosPerCate;
+}
